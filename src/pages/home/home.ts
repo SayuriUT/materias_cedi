667ef06767fe82d50750fa3e8ac82183360c90a0
alumno Secundaria <al3201@cedi.local>
2018-10-01 08:46:40 -0500

    crear papps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistoriaPage } from '../historia/historia';
 import { DibujoPage } from '../dibujo/dibujo';
+import { PappsPage } from '../papps/papps';
 
 @Component({
   selector: 'page-home',
@@ -10,6 +11,7 @@ import { DibujoPage } from '../dibujo/dibujo';
 export class HomePage {
   historia = HistoriaPage;
   dibujo = DibujoPage;
+  papps = PappsPage;
 
   constructor(public navCtrl: NavController) {
 
@@ -22,4 +24,7 @@ export class HomePage {
     this.navCtrl.push(this.dibujo);
   }
 
+  clickPapps(){
+    this.navCtrl.push(this.dibujo);
+  }
 }
